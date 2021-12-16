bf0b2cf08aa78af292f70a02150176208533fbde
NepNep21 <43792621+NepNep21@users.noreply.github.com>
2021-12-16 20:51:31 -0300

    Better CSS and attempt to fix edge cases

--- src/App.svelte
+++ src/App.svelte
@@ -10,62 +10,55 @@
   <h4>
     Hello, i'm nep (not real name), i'm 15, pan, male, and i spend most of my time playing games or working on projects :3
   </h4>
-  <!--viewBox from rain, AGPL-->
-  <a href="https://github.com/NepNep21" title="My github">
-    <svg viewBox="0 0 24 24" width=64px height=64px style="filter: invert(100%);">
-      <path d={mdiGithub}>
-    </svg>
-  </a>
+  <div style="flex-direction: row;">
+    <!--viewBox from rain, AGPL-->
+    <a href="https://github.com/NepNep21" title="My github">
+      <svg viewBox="0 0 24 24" width=64px height=64px>
+        <path d={mdiGithub}>
+      </svg>
+    </a>
 
-  <a href="https://steamcommunity.com/profiles/76561198303469579" title="My steam">
-    <svg viewBox="0 0 24 24" width=64px height=64px>
-      <path d={mdiSteam}>
-    </svg>
-  </a>
+    <a href="https://steamcommunity.com/profiles/76561198303469579" title="My steam">
+      <svg viewBox="0 0 24 24" width=64px height=64px>
+        <path d={mdiSteam}>
+      </svg>
+    </a>
 
-  <button on:click={discordClipboard} title="Copy discord tag">
-    <svg viewBox="0 0 24 24" width=64px height=64px>
-      <path d={mdiDiscord}>
-    </svg>
-  </button>
+    <button on:click={discordClipboard} title="Copy discord tag">
+      <svg viewBox="0 0 24 24" width=64px height=64px>
+        <path d={mdiDiscord}>
+      </svg>
+    </button>
+  </div>
 </main>
 
 <style>
   main {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
     display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
   }
 
   h4 {
     color: white;
-    /*:trollcrazy:*/
-    flex-basis: 100%;
+    font-weight: bold;
+    margin: 0 1em;
     white-space: nowrap;
   }
 
-  @media (max-width: 50em) {
+  @media only screen and (max-width: 50em) {
     h4 {
-      color: white;
-      /*:trollcrazy:*/
-      flex-basis: 100%;
       white-space: normal;
     }
   }
-  
-  /* Not needed
-  #center a {
-    display: inline;
-  }*/
 
   button {
     cursor: pointer;
     background-color: #212121;
     border: none;
+  }
+
+  svg {
+    filter: invert(80%);
   }
 </style>
