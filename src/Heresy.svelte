7ed1c7013a632892f4a5aa8771f57025afefe305
NepNep21 <43792621+NepNep21@users.noreply.github.com>
2023-06-22 10:49:13 -0300

    Less cryptic

--- src/Heresy.svelte
+++ src/Heresy.svelte
@@ -1,17 +1,33 @@
 <main>
-  <h1>I am Sofia, I am a heretic, no doubt inspired by a highly enlightened man. 
-  I shall not take part in "the dominant narrative" anymore, nor believe it as I did for far too long.</h1>
+  <h1>
+    The first draft of this page was far too cryptic, cryptic enough that it would turn people away from a
+    cause already widely viewed as "creepy". I've tried to improve it but unfortunately being completely
+    clear isn't possible either, otherwise this wouldn't be necessary to begin with.
+  </h1>
   <br/>
-  <p>It is quite interesting, but also depressing, how entire societies can turn on a group of mostly innocent people in only a century. 
-  But I am fairly certain change is coming, I am less certain of when it will come, but I find it likely it would be, to some extent, in my lifetime, 
-  maybe even my youth.</p>
+
+  <h1>
+    I am Sofia, in 2022 I was finally able to break out of the harmful "dominant narrative" regarding a
+    specific subject. In mid 2023 I was inspired by a self proclaimed heretic's blog, this page should
+    currently serve as a form of discovery and/or verification for my new website.
+  </h1>
   <br/>
-  <p>I feel it is my duty to do my best in this regard, even if it doesn't amount to much. 
-  If you know what this page means, you are not alone, you are not evil. 
-  Although I am not one of you, I can, to some extent, understand your struggle, and I sincerely hope for the best.</p>
+  <p>
+    It is quite interesting, but also depressing, how entire societies can turn on a group of mostly innocent people in only a century. 
+    But I am fairly certain change is coming, I am less certain of when it will come, but I find it likely it would be, to some extent, in my lifetime, 
+    maybe even my youth.
+  </p>
   <br/>
-  <p>This is not a link for the time being, for reasons that may, or not, be clear, if and when the time comes, maybe it will become one. 
-  If you found this after visiting elsewhere, yes I am who I claim to be. If you find something else from this, I hope you feel welcomed.</p>
+  <p>
+    I feel it is my duty to do my best in this regard, even if it doesn't amount to much. 
+    If you know what this page means, you are not alone, you are not evil. 
+    Although I am not one of you, I can, to some extent, understand your struggle, and I sincerely hope for the best.
+  </p>
+  <br/>
+  <p>
+    This is not a link for the time being, for reasons that may, or not, be clear,
+    if and when the time comes, maybe it will become one. 
+  </p>
   <br/>
   <p>The other side is not ready yet, check again next month.</p>
 </main>
@@ -19,5 +35,6 @@
 <style>
   h1, p {
     color: white;
+    text-align: center;
   }
 </style>
